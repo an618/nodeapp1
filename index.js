5a40a304b7dde25ae4f63a7feb3d65f584302ec5
Ankit <sahu19ankit@gmail.com>
2023-10-09 12:30:35 +0530

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-res.send("hello Ankit");
+res.send("hello Rahul");
 });
 //listen to port 80 by default
 app.listen(process.env.PORT || 80);
